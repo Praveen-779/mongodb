8919be627b4fef48292803665ee029547c8cc6ef
Praveen <Praveenpravi779@gmail.com>
2024-02-14 14:39:43 +0530

    Relations between Tables

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('65cc40038208a6ea72802675')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name,user.email,user.cart,user._id);
       next();
     })
     .catch(err => console.log(err));
